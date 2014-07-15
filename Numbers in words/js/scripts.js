37072f8d8cf1f5974011b45bd25d5d1174236841
Ruben Rhodes <student@epicodus.com>
2014-07-15 16:58:53 -0700

    end of class day. still requires modificaiton to deal with all types of numbers

--- Numbers in words/js/scripts.js
+++ Numbers in words/js/scripts.js
@@ -1,40 +1,81 @@
 var numbersInWords = function(number) {
   var splitNumber = number.toString().split("");
-  var units = {1:"one?", 2:"two?", 3:"three?", 4:"four?", 5:"five?", 6:"six?", 7:"seven?", 8:"eight?", 9:"nine?", 0:""};
-  var tens = {1:"ten?", 2:"twenty?", 3:"thirty?", 4:"fourty?", 5:"fifty?", 6:"sixty?", 7:"seventy?", 8:"eighty?", 9:"ninety?", 0:""};
-  var teens = {1:"eleven?", 2:"twelve?", 3:"thirteen?", 4:"fourteen?", 5:"fifteen?", 6:"sixteen?", 7:"seventeen?", 8:"eightteen?", 9:"nineteen?", 0:""};
-
+  var units = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine", 0:""};
+  var tens = {1:"ten", 2:"twenty", 3:"thirty", 4:"fourty", 5:"fifty", 6:"sixty", 7:"seventy", 8:"eighty", 9:"ninety", 0:""};
+  var teens = {1:"eleven", 2:"twelve", 3:"thirteen", 4:"fourteen", 5:"fifteen", 6:"sixteen", 7:"seventeen", 8:"eightteen", 9:"nineteen", 0:""};
+  var suffix = {1:"thousand", 2:"million", 3:"billion", 4:"trillion"}
 
   var numberPosition = "units";
   var wordResivouir = [];
+  var wordsInserted = 0;
+  var count = 0;
+  var suffixToInsert = 1;
+
+  if (number === 0) {
+    return "zero"
+  }
 
   for (var i = splitNumber.length-1; i > -1; i--) {
+
     if (numberPosition === "units") {
-    wordResivouir = wordResivouir + units[parseInt(splitNumber[i])];
-    numberPosition = "tens";
+      wordResivouir.push(units[parseInt(splitNumber[i])]);
+      numberPosition = "tens";
+      wordsInserted++;
 
     } else if (numberPosition === "tens") {
-
       if (parseInt(splitNumber[i]) > 1) {
-        wordResivouir = wordResivouir + tens[parseInt(splitNumber[i])];
+        wordResivouir.push(tens[parseInt(splitNumber[i])]);
+        wordsInserted++;
       } else if (parseInt(splitNumber[i]) === 1 && parseInt(splitNumber[i+1]) === 0) {
-        wordResivouir = wordResivouir + tens[parseInt(splitNumber[i])];
+        wordResivouir.push(tens[parseInt(splitNumber[i])]);
+        wordsInserted++;
       } else if (parseInt(splitNumber[i]) === 1 && parseInt(splitNumber[i+1]) > 0) {
-        console.log(wordResivouir)
-        wordResivouir = wordResivouir.slice(wordResivouir.length) + teens[parseInt(splitNumber[i +1])];
-        console.log(wordResivouir)
+        wordResivouir.pop();
+        wordResivouir.push(teens[parseInt(splitNumber[i +1])]);
+        wordsInserted++;
     };
 
     numberPosition = "hundreds";
 
     } else if (numberPosition === "hundreds") {
-      wordResivouir = wordResivouir + units[parseInt(splitNumber[i])];
-      numberPosition = "units";
 
+      wordResivouir.push("hundred")
+      wordResivouir.push(units[parseInt(splitNumber[i])]);
+      numberPosition = "units";
+      wordsInserted++;
     };
 
+    count++
+    if (count % 3 === 0 && i > 0) {
+      wordResivouir.push(suffix[suffixToInsert]);
+      count = 0
+      suffixToInsert++
+    };
   };
-  wordResivouir = wordResivouir.split("?").reverse().join(" ").trim()
-return wordResivouir
 
+
+
+  for (i = 0; i < wordResivouir.length; i++) {
+    if (wordResivouir[i] === "hundred" && wordResivouir[i-1] === "" && wordResivouir[i+1] === "") {
+      wordResivouir[i] = "";
+    }
+  }
+
+  for (i = 0; i < wordResivouir.length; i++) {
+    if (wordResivouir[i] === "thousand" && wordResivouir[i+1] === "" && wordResivouir[i+2] === "") {
+      wordResivouir[i] = "";
+    }
+  }
+
+  console.log(wordResivouir);
+
+  for (var i = wordResivouir.length; i > -1; i--) {
+    if (wordResivouir[i] === "") {
+      wordResivouir.splice(i,1)
+    };
+  };
+
+
+  var result = wordResivouir.reverse().join(" ").trim();
+  return result;
 };
--- Numbers in words/specs/specs.js
+++ Numbers in words/specs/specs.js
@@ -1,29 +1,70 @@
 describe("numbersInWords",function(){
-  it("1 equals one",function(){
+  it("1 should return one",function(){
   numbersInWords(1).should.equal("one");
   });
 
-  it("22 equals twenty two", function(){
+  it("22 should return twenty two", function(){
     numbersInWords(23).should.equal("twenty three");
   });
-  it("451 equals to four hundred fifty one", function(){
+
+  it("451 should return four hundred fifty one", function(){
     numbersInWords(451).should.equal("four hundred fifty one");
   });
-  it("1342 equals one thousand three hundred fourty two",function(){
+
+  it("1342 should return one thousand three hundred fourty two",function(){
     numbersInWords(1342).should.equal("one thousand three hundred fourty two")
   });
+
   it("10 should return ten",function(){
     numbersInWords(10).should.equal("ten")
   });
-  it("14 should return 14",function(){
+
+  it("14 should return fourteen",function(){
     numbersInWords(14).should.equal("fourteen")
   });
-  it("405 equals to four hundred five", function(){
+
+  it("405 should return four hundred five", function(){
     numbersInWords(405).should.equal("four hundred five");
   });
-    it("212817 equals to two one two eight one seven", function(){
-    numbersInWords(212817).should.equal("two one two eight one seven");
+
+  it("212817 should return two hundred twelve eight hundred seventeen", function(){
+    numbersInWords(212817).should.equal("two hundred twelve thousand eight hundred seventeen");
   });
 
+  it("8212343 should return eight million two hundred twelve thousand three hundred fourty three", function(){
+    numbersInWords(8212343).should.equal("eight million two hundred twelve thousand three hundred fourty three");
+  });
+
+  it("200 should return two hundred", function(){
+    numbersInWords(200).should.equal("two hundred");
+  });
+
+  it("0 should return zero", function(){
+    numbersInWords(0).should.equal("zero");
+  });
+
+  it("4000 should return four thousand", function(){
+    numbersInWords(4000).should.equal("four thousand");
+  });
+
+  it("40000 should return fourty thousand", function(){
+    numbersInWords(40000).should.equal("fourty thousand");
+  });
+
+  it("400000 should return four hundred thousand", function(){
+    numbersInWords(400000).should.equal("four hundred thousand");
+  });
+
+  it("4000000 should return four million", function(){
+    numbersInWords(4000000).should.equal("four million");
+  });
+
+  it("40000000 should return fourty million", function(){
+    numbersInWords(40000000).should.equal("fourty million");
+  });
+
+  it("4001 should return four thousand one", function(){
+    numbersInWords(4001).should.equal("four thousand one");
+  });
 });
 
